bc6c7312cd74d812a7c195d545afecc254787d61
Vasiliy Poperekov <vasya.poperekov@mail.ru>
2021-06-05 13:45:21 +0300

    Homework 6 android

--- app/src/main/java/com/example/homeworks/MainActivity.kt
+++ app/src/main/java/com/example/homeworks/MainActivity.kt
@@ -2,20 +2,21 @@ package com.example.homeworks
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.homeworks.homework6.Homework6Activity as Homework6Activity
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var clickableText: TextView
+    lateinit var btnToHm6: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        clickableText = findViewById(R.id.main_text)
-        clickableText.setOnClickListener {
+        btnToHm6 = findViewById(R.id.bv_btn_to_homework6)
+        btnToHm6.setOnClickListener {
             val intent = Intent(this, Homework6Activity::class.java)
             startActivity(intent)
         }
